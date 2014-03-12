cb0f3e76449aabf50e2daff83944b937e7b08eee
chao-xian <dev@kelvingan.co.uk>
2014-03-12 17:29:11 +0000

    Consistently using ENV vars and dumping snippets and fragments into separate folder to keep root folder tidier.

--- generate-search-report.rb
+++ generate-search-report.rb
@@ -8,50 +8,54 @@ require 'open-uri'
 
 # Set up HTTP client to get report page and get past HTTP Auth
 http = Net::HTTP.new(ENV['FUNNELBACK_URL'],8443)
-req = Net::HTTP::Get.new("/search/admin/analytics/Dashboard?atab=Analyse&collection=website")
+report_url = "/search/admin/analytics/Dashboard?collection=website&time=tq&timeframe=day&startDate=20131212&endDate=20140312"
+req = Net::HTTP::Get.new(report_url)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 req.basic_auth(ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD'])
 response = http.request(req)
 
 # Save to disk so that Nokogiri can view it
-open("website-report.html", "wb") { |file|
+open("snippets/website-report.html", "wb") { |file|
     file.write(response.body)
 }
 
 # Attempt to get the Usage summary chart
 # Go to page that has chart and link to download
-req = Net::HTTP::Get.new('/search/admin/analytics/Dashboard?r=SUMMARY&collection=website&time=l90d&timeframe=day&startDate=20131211&endDate=20140311')
-req.basic_auth ENV['FUNNELBACK_USER'], ENV['FUNNELBACK_PW']
+req = Net::HTTP::Get.new('/search/admin/analytics/Dashboard?startDate=20140101T000000&endDate=20140401T000000&time=p&timeframe=quarter&collection=website&profile=&from=outliers&r=SUMMARY')
+req.basic_auth ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD']
 response = http.request(req)
 
-open("usage-summary-chart.html", "wb") { |file|
+open("snippets/usage-summary-chart.html", "wb") { |file|
     file.write(response.body)
 }
 
 
-chart_page = File.open("usage-summary-chart.html")
+chart_page = File.open("snippets/usage-summary-chart.html")
 chart_doc = Nokogiri::HTML(chart_page)
 chart_page.close
 
 chart_url = chart_doc.css("div#download a")[2]['href'].gsub(/\s+/, "")
 
 req = Net::HTTP::Get.new('/search/admin/analytics/' + chart_url)
-req.basic_auth ENV['FUNNELBACK_USER'], ENV['FUNNELBACK_PW']
+req.basic_auth ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD']
 response = http.request(req)
 
-open("usage-summary-chart.png", "wb") { |file|
+open("assets/usage-summary-chart.png", "wb") { |file|
     file.write(response.body)
 }
 
 # Look at the rest of the report
-report_page = File.open("website-report.html")
+report_page = File.open("snippets/website-report.html")
 doc = Nokogiri::HTML(report_page)
 report_page.close
 
 # Summary
 summary_table = doc.css('div.summary_report table')
-# puts summary_table
+puts summary_table
+open("assets/sumary-table.html", "wb") { |file|
+    file.write(summary_table)
+}
 
 
 # get '/' do
