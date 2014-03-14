79a5410b009b16b5c99eacb198ddd2c85dbd3b31
chao-xian <dev@kelvingan.co.uk>
2014-03-14 12:43:40 +0000

    Refactored initialisation of HTTP client and one more passing test

--- generate_search_report.rb
+++ generate_search_report.rb
@@ -11,6 +11,8 @@ class GenerateFunnelbackReport
 
     FUNNELBACK_REPORT_HTML = "report.html"
     MONTHLY_USAGE_FILENAME = "monthly-usage-table.html"
+    USAGE_CHART_HTML = "usage-summary-chart.html"
+    USAGE_CHART = "usage-summary-chart.png"
 
     def initialize(output_location)
         if !output_location.end_with? "/"
@@ -22,32 +24,54 @@ class GenerateFunnelbackReport
             FileUtils.mkdir(@output_location)
         end
 
-    end
-
-    def fetch_monthly_usage(report_url)
-        self.fetch_funnelback_content(report_url, MONTHLY_USAGE_FILENAME)
-
-        monthly_usage_table = @doc.css('div.summary_report table')[0]
-        open(@output_location + MONTHLY_USAGE_FILENAME, "wb") { |file|
-            file.write(monthly_usage_table)
-        }
+        # Set up HTTP client to get report page and get past HTTP Auth
+        @http = Net::HTTP.new(ENV['FUNNELBACK_URL'],8443)
+        @http = Net::HTTP.new(ENV['FUNNELBACK_URL'],8443)
+        @http.use_ssl = true
+        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
 
     def get_monthly_usage_filename
         MONTHLY_USAGE_FILENAME
     end
 
+    def get_usage_chart_filename
+        USAGE_CHART
+    end
 
-    def fetch_funnelback_content(url, output_filename)
+    def fetch_monthly_usage(report_url)
 
-        # Set up HTTP client to get report page and get past HTTP Auth
-        http = Net::HTTP.new(ENV['FUNNELBACK_URL'],8443)
-        report_url = "/search/admin/analytics/Dashboard?collection=website&time=tq&timeframe=day&startDate=20131212&endDate=20140312"
+        self.fetch_funnelback_content(report_url, MONTHLY_USAGE_FILENAME)
+
+        monthly_usage_table = @doc.css('div.summary_report table')[0]
+        open(@output_location + MONTHLY_USAGE_FILENAME, "wb") { |file|
+            file.write(monthly_usage_table)
+        }
+
+    end
+
+    def fetch_usage_chart(report_url)
+
+        self.fetch_funnelback_content(report_url, USAGE_CHART_HTML)
+
+        chart_url = @doc.css("div#download a")[2]['href'].gsub(/\s+/, "")
+
+        req = Net::HTTP::Get.new('/search/admin/analytics/' + chart_url)
+        req.basic_auth ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD']
+        response = @http.request(req)
+
+        open(@output_location + USAGE_CHART, "wb") { |file|
+            file.write(response.body)
+        }
+
+    end
+
+    def fetch_funnelback_content(report_url, output_filename)
+
+        # report_url = "/search/admin/analytics/Dashboard?collection=website&time=tq&timeframe=day&startDate=20131212&endDate=20140312"
         req = Net::HTTP::Get.new(report_url)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         req.basic_auth(ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD'])
-        response = http.request(req)
+        response = @http.request(req)
 
         # Save to disk so that Nokogiri can view it
         open(@output_location + FUNNELBACK_REPORT_HTML, "wb") { |file|
@@ -57,33 +81,6 @@ class GenerateFunnelbackReport
         report_page = File.open(@output_location + FUNNELBACK_REPORT_HTML)
         @doc = Nokogiri::HTML(report_page)
         report_page.close
-
-        # Attempt to get the Usage summary chart
-        # Go to page that has chart and link to download
-        # req = Net::HTTP::Get.new('/search/admin/analytics/Dashboard?startDate=20140101T000000&endDate=20140401T000000&time=p&timeframe=quarter&collection=website&profile=&from=outliers&r=SUMMARY')
-        # req.basic_auth ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD']
-        # response = http.request(req)
-
-        # open("snippets/usage-summary-chart.html", "wb") { |file|
-        #     file.write(response.body)
-        # }
-
-
-        # chart_page = File.open("snippets/usage-summary-chart.html")
-        # chart_doc = Nokogiri::HTML(chart_page)
-        # chart_page.close
-
-        # chart_url = chart_doc.css("div#download a")[2]['href'].gsub(/\s+/, "")
-
-        # req = Net::HTTP::Get.new('/search/admin/analytics/' + chart_url)
-        # req.basic_auth ENV['FUNNELBACK_USERNAME'], ENV['FUNNELBACK_PASSWORD']
-        # response = http.request(req)
-
-        # open("assets/usage-summary-chart.png", "wb") { |file|
-        #     file.write(response.body)
-        # }
-
-        # Look at the rest of the report
 
     end
 
--- test-generate-search-report.rb
+++ test-generate-search-report.rb
@@ -8,7 +8,9 @@ class TestGenerateFunnelbackReport < MiniTest::Unit::TestCase
 
   def setup
     @output_location = "testoutput"
-    @funnelbacl_quarterly_report_url = "/search/admin/analytics/Dashboard?collection=website&time=tq&timeframe=day&startDate=20131212&endDate=20140312"
+    # TODO this would be better if the URL was hidden in the initialiser, and took date range
+    @funnelback_quarterly_report_url = "/search/admin/analytics/Dashboard?collection=website&time=tq&timeframe=day&startDate=20131212&endDate=20140312"
+    @funnelback_top_queries_chart_url = "/search/admin/analytics/Dashboard?startDate=20140101T000000&endDate=20140401T000000&time=p&timeframe=quarter&collection=website&profile=&from=outliers&r=SUMMARY"
     @generator = GenerateFunnelbackReport.new(@output_location)
     # should probably check that output_location is empty
   end
@@ -22,9 +24,15 @@ class TestGenerateFunnelbackReport < MiniTest::Unit::TestCase
     assert(File.exists?(@output_location + "/" + @generator.get_monthly_usage_filename))
   end
 
+  def test_usage_chart_exists
+    @generator.fetch_usage_chart(@funnelback_top_queries_chart_url)
+    assert(File.exists?(@output_location + "/" + @generator.get_usage_chart_filename))
+  end
+
   # def test_top_queries_chart_exists
-  #   flunk("test hasn't been written yet")
+
   # end
+
 
   # def test_result_clicks_exists
   #   flunk("test hasn't been written yet")
